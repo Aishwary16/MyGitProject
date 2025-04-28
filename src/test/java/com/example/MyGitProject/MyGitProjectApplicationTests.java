affb059fdafeedd72b3c7bd2713457e56ae08d93
Aishwary Khare <aishwary.khare@newgensoft.com>
2025-04-28 11:29:18 +0530

    Initial commit

---
+++ src/test/java/com/example/MyGitProject/MyGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyGitProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
