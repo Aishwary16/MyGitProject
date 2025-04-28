0b91b5c89bb8d1a9cdfeeff255bb07fde10c68ad
Aishwary <aishwary1604@gmail.com>
2025-04-28 16:34:50 +0530

    commited by akshat

--- src/main/java/com/example/MyGitProject/Controller/Mycontoller.java
+++ src/main/java/com/example/MyGitProject/Controller/Mycontoller.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Mycontoller {
     @GetMapping("/hello")
 public String hello(){
-        return "Hello from shahank";
+        return "Hello from akshat";
     }
 }
